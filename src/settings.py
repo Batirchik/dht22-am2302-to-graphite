8b132aa874a5e7cfa3990fa2214d30e62087f729
yazovsky <yazovsky@gmail.com>
2018-01-14 00:33:11 -0500

    add env variables for config

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,12 @@
-graphite_server = "raspberry"
+import os
+
+graphite_server = "localhost"
 graphite_port = 2003
 pin_number = 4
 retry_times = 10
+
+
+if os.environ['GRAPHITE_SERVER']:
+	graphite_server = os.environ['GRAPHITE_SERVER']
+if os.environ['GRAPHITE_PORT']:
+	graphite_port = os.environ['GRAPHITE_PORT']

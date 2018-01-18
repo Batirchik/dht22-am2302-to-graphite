77cfab564a442d8e96ca4c809f726a8576a67081
Anton Yazovskiy <yazovsky@gmail.com>
2018-01-17 23:20:27 -0500

    auto-start flask app in script. Don't rely on external variables

--- utils/grafana_webhook_handler.py
+++ utils/grafana_webhook_handler.py
@@ -1,6 +1,11 @@
 from flask import Flask
 from flask import request
 import os
+import logging
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format="%(asctime)s - %(filename)s:%(funcName)s:%(lineno)d - %(levelname)s - %(message)s")
 
 
 app = Flask(__name__)
@@ -9,6 +14,8 @@ app = Flask(__name__)
 @app.route('/webhook', methods=['POST', 'PUT'])
 def webhook():
 	json = request.json
+
+	logging.debug(json)
 
 	if json['state'] == "alerting":
 		# just because I can.
@@ -19,3 +26,7 @@ def webhook():
 		# Don't care why and too lazy to figure this out. So, I have to restart the process.
 		os.system("sudo service sensors-to-grafana restart")
 	return 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
